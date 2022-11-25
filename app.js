d62c5b5b36d34756a30356aee3134ddd9140163b
Jackson <Jackson@KewSS>
2022-11-25 13:14:18 +0800

    Some code

--- app.js
+++ app.js
@@ -3,3 +3,7 @@
  * This is a sample node script.
  * 
  */
+const express = require('express');
+const moment = require('moment');
+console.log('Hello there');
+console.log(`Time now is ${moment().date()}`);
