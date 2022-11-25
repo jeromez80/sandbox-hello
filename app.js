7d8659a189b45790e76e1050428c5599ce7557da
Jackson <Jackson@KewSS>
2022-11-25 13:19:24 +0800

    Make a express router demo code.

--- app.js
+++ app.js
@@ -7,3 +7,25 @@ const express = require('express');
 const moment = require('moment');
 console.log('Hello there');
 console.log(`Time now is ${moment().date()}`);
+
+const router=express.Router()
+
+// Handling request using router
+router.get("/home",(req,res,next)=>{
+    res.send("This is the homepage request")
+})
+
+// Handling login request
+router.get("/login",(req,res,next)=>{
+    res.send("This is the login request")
+  })
+  
+
+const app=express()
+  
+// Handling routes request
+app.use("/",router)
+
+app.listen((3000),()=>{
+    console.log("Server is Running")
+})
